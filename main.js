ac65db90c8b05b397f6a854bb21b2f3aef3ab695
kaelith-co <sagarchuhan3800@gmail.com>
2026-01-22 10:26:13 +0530

    Delete main.js

--- main.js
+++
@@ -1,7 +0,0 @@
-console.log('Hello World!');
-const glow = document.querySelector(".cursor-glow");
-
-document.addEventListener("mousemove", (e) => {
-  glow.style.left = e.clientX + "px";
-  glow.style.top = e.clientY + "px";
-});
